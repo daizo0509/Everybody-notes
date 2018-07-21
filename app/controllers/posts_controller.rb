a20eae1f341bb5be31f7d24309cb5d7367272146
daizo0509 <yuya.ohkura.10@gmail.com>
2018-07-21 08:38:37 +0000

    Postモデル・コントローラーを作り投稿・編集・削除をできるようにした。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,49 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
   def index
+    @posts = Post.all
   end
 
   def show
+    
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
+    @post = Post.find(params[:id])
+
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to("/")
+    
   end
 
   def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        redirect_to post_path(@post.id)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
   end
+
+  private
+
+  def set_post
+      @post = Post.find(params[:id])
+  end
+
+  def post_params
+      params.require(:post).permit(:title, :content )
+  end
+
 end
--- db/migrate/20180721070915_create_posts.rb
+++ db/migrate/20180721070915_create_posts.rb
@@ -1,10 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :content
-      t.references :user, foreign_key: true
-      t.integer :likes_count , default: 0
+      t.string :title,null: false
+      t.text :content,null: false
 
       t.timestamps
     end
