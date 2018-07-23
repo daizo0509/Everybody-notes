e5eb2f4378284b44dc9a1b4eac9957e58ac87d61
daizo0509 <yuya.ohkura.10@gmail.com>
2018-07-23 04:39:30 +0000

    Likeモデル作成Postモデルにlikes_countカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_113729) do
+ActiveRecord::Schema.define(version: 2018_07_23_042742) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "use_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["use_id"], name: "index_likes_on_use_id"
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_07_21_113729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "likes_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
