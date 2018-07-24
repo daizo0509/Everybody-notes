c9a70aeb89645924731154af7ef933fa69d77404
daizo0509 <yuya.ohkura.10@gmail.com>
2018-07-24 11:22:34 +0000

    Tag機能（登録）追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_042742) do
+ActiveRecord::Schema.define(version: 2018_07_23_092944) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -39,6 +39,22 @@ ActiveRecord::Schema.define(version: 2018_07_23_042742) do
     t.integer "user_id"
     t.integer "likes_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_taggings_on_post_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "tag_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
