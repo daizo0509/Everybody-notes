d6229ae2cd5a65e87e322b05dbd0065b9ab6f8b1
daizo0509 <yuya.ohkura.10@gmail.com>
2018-08-04 08:40:01 +0000

    プロフィール画像機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_172937) do
+ActiveRecord::Schema.define(version: 2018_08_04_083430) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -87,6 +87,9 @@ ActiveRecord::Schema.define(version: 2018_08_02_172937) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_images"
+    t.text "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
