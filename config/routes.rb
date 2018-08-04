d5921f47f2e662de66b839a26513185a9fbfd9d5
daizo0509 <yuya.ohkura.10@gmail.com>
2018-08-04 04:11:34 +0000

    コメント機能の削除・編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 	root 'posts#index'
 	resources :users
 	resources :posts do
-		resource :post_comments, only: [:create, :destroy]
+		resources :post_comments
 		resource :likes, only: [:create, :destroy]
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
